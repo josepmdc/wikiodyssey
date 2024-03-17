743c0e59dacbb7b32b87f51991d84852c430a531
MartinKpl <martin.kaplan910@gmail.com>
2024-03-17 09:58:39 +0200

    IsTitleInArticle implemented

--- lib/main.go
+++ lib/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 /*
@@ -11,12 +12,13 @@ import (
 	https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=2&rnnamespace=0
 2. Search for titles based on input:
 	https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={next_node}&utf8=&format=json&rnnamespace=0
-3. Check if title of next node is linked in current node
+3. Check if title of next node is linked in current node (Current_node has to be exact title, caps included)
 	https://en.wikipedia.org/w/api.php?action=query&titles={current_node}&prop=links&pltitles={next_node}&format=json
 */
 
 const randomApiURI = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0"
 const searchTitlesURI = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=%s&utf8=&format=json"
+const articleLinksURI = "https://en.wikipedia.org/w/api.php?action=query&titles=%s&prop=links&pltitles=%s&format=json&pllimit=max&plnamespace=0"
 
 type RandomQuery struct {
 	Query *WikiRandom `json:"query"`
@@ -28,6 +30,23 @@ type WikiRandom struct {
 
 type RandomEntry struct {
 	Title string `json:"title"`
+}
+
+func GetRandomArticles(n uint) ([]string, error) {
+	resp, err := http.Get(fmt.Sprintf("%s&rnlimit=%d", randomApiURI, n))
+	if err != nil {
+		return nil, fmt.Errorf("error fetching random articles: %w", err)
+	}
+
+	var randomResp RandomQuery
+	err = json.NewDecoder(resp.Body).Decode(&randomResp)
+
+	titles := make([]string, n)
+	for i, entry := range randomResp.Query.Random {
+		titles[i] = entry.Title
+	}
+
+	return titles, nil
 }
 
 type SearchTitleQuery struct {
@@ -50,26 +69,9 @@ type SearchEntry struct {
 // 	TotalHits uint `json:"totalhits"`
 // }
 
-func GetRandomArticles(n uint) ([]string, error) {
-	resp, err := http.Get(fmt.Sprintf("%s&rnlimit=%d", randomApiURI, n))
-	if err != nil {
-		return nil, fmt.Errorf("error fetching random articles: %w", err)
-	}
-
-	var randomResp RandomQuery
-	err = json.NewDecoder(resp.Body).Decode(&randomResp)
-
-	titles := make([]string, n)
-	for i, entry := range randomResp.Query.Random {
-		titles[i] = entry.Title
-	}
-
-	return titles, nil
-}
-
 func GetTitles(input string) (string, error) {
-	resp, err := http.Get(fmt.Sprintf(searchTitlesURI, input)) //replace spaces?
-
+	input = strings.ReplaceAll(input, " ", "_")
+	resp, err := http.Get(fmt.Sprintf(searchTitlesURI, input))
 	if err != nil {
 		return "", fmt.Errorf("error fetching random articles: %w", err)
 	}
@@ -86,9 +88,59 @@ func GetTitles(input string) (string, error) {
 	return title, nil
 }
 
-func main() {
-	title, _ := GetTitles("einstein")
+type CheckerQuery struct {
+	Query struct {
+		Pages map[string]struct {
+			Links []*CheckerEntry `json:"links"`
+		} `json:"pages"`
+	} `json:"query"`
+}
 
+type CheckerEntry struct {
+	Title string `json:"title"`
+}
+
+func IsTitleInArticle(sourceTitle string, targetTitle string) (string, error) {
+	sourceTitle = strings.ReplaceAll(sourceTitle, " ", "_")
+	originalTargetTitle := targetTitle
+	targetTitle = strings.ReplaceAll(targetTitle, " ", "_")
+	resp, err := http.Get(fmt.Sprintf(articleLinksURI, sourceTitle, targetTitle)) //replace spaces?
+
+	if err != nil {
+		return "", fmt.Errorf("error fetching random articles: %w", err)
+	}
+
+	var checkerQuery CheckerQuery
+	err = json.NewDecoder(resp.Body).Decode(&checkerQuery)
+
+	links := make([]*CheckerEntry, 1)
+
+	for _, value := range checkerQuery.Query.Pages {
+		if len(value.Links) == 1 {
+			return value.Links[0].Title, nil
+		} else {
+			links = value.Links
+		}
+
+		break
+	}
+
+	for _, link := range links {
+		if link.Title == originalTargetTitle {
+			return link.Title, nil
+		}
+	}
+
+	return "", fmt.Errorf("no matching titles found")
+}
+
+func main() {
+	// title, _ := GetTitles("albert einstein")
+	title, err := IsTitleInArticle("Albert einstein", "ulm")
+
+	if err != nil {
+		fmt.Printf("error: %w", err)
+	}
 	fmt.Println(title)
 
 }
